564c265d7f6c5d71c752deb5c681e7f84437bbd4
Micah Nyabuto <atembamicah@gmail.com>
2025-08-04 12:37:59 +0300

    Modified the colors

--- app/src/main/java/com/micahnyabuto/livespotevents/ui/theme/Color.kt
+++ app/src/main/java/com/micahnyabuto/livespotevents/ui/theme/Color.kt
@@ -3,10 +3,10 @@ package com.micahnyabuto.livespotevents.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryColor = Color(0xFFFF8C00)
+val PrimaryColor = Color(0xffBF36A1 )
 val PrimaryLightColor = PrimaryColor.copy(alpha = 0.75f)
 
-val SecondaryColor = Color(0xFFFF9800)
+val SecondaryColor = Color(0xffD987C3)
 val SecondaryLightColor = SecondaryColor.copy(alpha = 0.75f)
 
 val PrimaryTextColor = Color(0xffffffff)
@@ -14,11 +14,11 @@ val SecondaryTextColor = Color(0xff000000)
 
 val SurfaceDark = Color(0xff21182B)
 
-val SurfaceLight = Color(0xfff5f5f5)
+val SurfaceLight = Color(0xffF5F0F8)
 
-val BackgroundLightColor = Color(0xfff5f5f5)
+val BackgroundLightColor = Color(0xffF5F0F8)
 
-val BackgroundDarkColor = Color(0xff101010)
+val BackgroundDarkColor = Color(0xff21182B)
 
 val ErrorColor = Color(0xfff44336)
 val OnErrorColor = Color(0xff000000)
