5ea326b61d6be50c23ec51a55232eeb064d9e60d
Micah Nyabuto <atembamicah@gmail.com>
2025-08-15 00:04:19 +0300

    Added new destinations

--- app/src/main/java/com/micahnyabuto/livespotevents/core/navigation/MainGraph.kt
+++ app/src/main/java/com/micahnyabuto/livespotevents/core/navigation/MainGraph.kt
@@ -1,8 +1,115 @@
 package com.micahnyabuto.livespotevents.core.navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.micahnyabuto.livespotevents.features.create.CreateEventScreen
+import com.micahnyabuto.livespotevents.features.events.EventsScreen
+import com.micahnyabuto.livespotevents.features.explore.ExploreScreen
+import com.micahnyabuto.livespotevents.features.profile.ProfileScreen
 
 @Composable
-fun MainGraph(){
+fun MainGraph(
+    navController: NavHostController
+){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route.orEmpty()
+    val showBottomNavigation = currentRoute !in  listOf(
+        Destinations.Splash.route,
 
+    )
+    Scaffold (
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal),
+        bottomBar = {
+            if (showBottomNavigation) {
+                Column {
+                    HorizontalDivider(thickness = 2.dp)
+                    NavigationBar(
+                        tonalElevation = 0.dp,
+                        containerColor = MaterialTheme.colorScheme.surface
+                    ) {
+                        BottomNavigationItem.entries.forEach { navigationItem ->
+
+                            val isSelected = currentRoute == navigationItem.route
+
+                            NavigationBarItem(
+                                selected = isSelected,
+                                icon = {
+                                    Icon(
+                                        imageVector = if (isSelected) navigationItem.selectedIcon else navigationItem.unselectedIcon,
+                                        contentDescription = navigationItem.label
+                                    )
+                                },
+                                label = {
+                                    Text(
+                                        text = navigationItem.label,
+                                        style = MaterialTheme.typography.labelSmall.copy(
+                                            fontSize = 10.sp,
+                                            fontWeight = if (isSelected) FontWeight.SemiBold else FontWeight.Normal
+                                        )
+                                    )
+                                },
+                                onClick = {
+                                    if (currentRoute != navigationItem.route) {
+                                        navController.navigate(navigationItem.route)
+                                    }
+                                },
+                                colors = NavigationBarItemDefaults.colors(
+                                    indicatorColor = Color.Transparent,
+                                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
+    ){innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Destinations.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Destinations.Home.route) {
+                EventsScreen()
+            }
+            composable(Destinations.Explore.route) {
+                ExploreScreen()
+            }
+            composable(Destinations.Create.route) {
+                CreateEventScreen()
+            }
+            composable(Destinations.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
 }
