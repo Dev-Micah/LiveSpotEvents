033c9aa8df2a075f361b01c3df4386a3df5c971a
Micah Nyabuto <atembamicah@gmail.com>
2025-08-15 18:29:46 +0300

    Read images Permission

---
+++ app/src/main/java/com/micahnyabuto/livespotevents/core/permissions/ImagePermission.kt
@@ -0,0 +1,8 @@
+package com.micahnyabuto.livespotevents.core.permissions
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ImagesPermission(){
+    //val permissionState = rememberMultiplePermissionsState()
+}
