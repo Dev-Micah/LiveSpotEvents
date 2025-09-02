44bab4113cdbb298973f408bce3f9eef8d358eb4
Micah Nyabuto <atembamicah@gmail.com>
2025-09-02 22:31:09 +0300

    Added SupabaseClient to SupabaseModule

--- app/src/main/java/com/micahnyabuto/livespotevents/di/SupabaseModule.kt
+++ app/src/main/java/com/micahnyabuto/livespotevents/di/SupabaseModule.kt
@@ -1,5 +1,6 @@
 package com.micahnyabuto.livespotevents.di
 
+import com.micahnyabuto.livespotevents.core.data.supabase.SupabaseClientInstance
 import com.micahnyabuto.livespotevents.core.domain.EventsRepository
 import com.micahnyabuto.livespotevents.features.create.EventsViewModel
 import org.koin.core.module.dsl.viewModel
@@ -7,13 +8,17 @@ import org.koin.dsl.module
 
 val appModule = module {
     single {
+        SupabaseClientInstance
+    }
+
+    single {
         EventsRepository(
             client = get()
         )
     }
+    
 
     viewModel {
         EventsViewModel(get())
-
     }
 }
