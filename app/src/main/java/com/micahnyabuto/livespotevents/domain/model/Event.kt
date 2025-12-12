6a80add72eee5eae22976d193c2830f835e78a69
Dev-Micah <atembamicah@gmail.com>
2025-12-13 00:55:16 +0300

    Event id to take null

--- app/src/main/java/com/micahnyabuto/livespotevents/domain/model/Event.kt
+++ app/src/main/java/com/micahnyabuto/livespotevents/domain/model/Event.kt
@@ -1,10 +1,11 @@
 package com.micahnyabuto.livespotevents.domain.model
 
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class Event(
-    val id: Int? = null,
+    val id: String? =null,
     val title: String,
     val event_date: String,
     val event_time: String,
