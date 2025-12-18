b503d9f3bbb8a36f6cbf899c8aaf85b1ab6fc7b3
Dev-Micah <atembamicah@gmail.com>
2025-12-18 23:24:48 +0300

    Swipable Bottom bar

--- app/src/main/java/com/micahnyabuto/livespotevents/ui/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/micahnyabuto/livespotevents/ui/navigation/BottomNavigationItem.kt
@@ -1,45 +1,43 @@
 package com.micahnyabuto.livespotevents.ui.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddBox
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Explore
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.outlined.AddBox
 import androidx.compose.material.icons.outlined.AddCircleOutline
 import androidx.compose.material.icons.outlined.Explore
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.ui.graphics.vector.ImageVector
+import kotlin.reflect.KClass
 
-// This enum was missing. Your code depends on it for the bottom navigation.
 enum class BottomNavigationItem(
-    val route: String,
+    val route: KClass<*>,
     val label: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector
 ) {
     HOME(
-        route = Destinations.Home.route,
+        route = Home::class,
         label = "Home",
         selectedIcon = Icons.Filled.Home,
         unselectedIcon = Icons.Outlined.Home
     ),
     EXPLORE(
-        route = Destinations.Explore.route,
+        route = Explore::class,
         label = "Explore",
         selectedIcon = Icons.Filled.Explore,
         unselectedIcon = Icons.Outlined.Explore
     ),
     CREATE(
-        route = Destinations.Create.route,
+        route = Create::class,
         label = "Create",
         selectedIcon = Icons.Filled.AddCircle,
         unselectedIcon = Icons.Outlined.AddCircleOutline
     ),
     PROFILE(
-        route = Destinations.Profile.route,
+        route = Profile::class,
         label = "Profile",
         selectedIcon = Icons.Filled.Person,
         unselectedIcon = Icons.Outlined.Person
